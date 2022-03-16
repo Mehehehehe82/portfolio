e131fb145ba0d8b5739e51d6d7e2d458b2757471
Evan Bishop <evanpbishop@gmail.com>
2022-03-15 21:47:36 -0400

    Got the stuff working

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders "What is fair use?"', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/What is Fair Use/i);
   expect(linkElement).toBeInTheDocument();
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,38 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
-function App() {
+import data from './data.json';
+import { LoremIpsum } from "lorem-ipsum";
+const lorem = new LoremIpsum({
+  sentencesPerParagraph: {
+    max: 4,
+    min: 3
+  },
+  wordsPerSentence: {
+    max: 7,
+    min: 6
+  }
+});
+function App(): JSX.Element {
+	/** Current artifact id */
+	let key = 1
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Evan Bishop
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+			{data.data.map((dat:any) => (
+			<div className="App-section" key={key++}>
+				<h1>Artifact {key}</h1>
+				<div>
+					<h2>{dat.head||"missing head"}</h2>
+					<p>{dat.text||("Missing \"text\" attribute. "+lorem.generateParagraphs(1))}</p>
+					{dat.imageURI && <img src={dat.imageURI} alt="" />}
+				</div>
+			</div>))}
     </div>
   );
 }
